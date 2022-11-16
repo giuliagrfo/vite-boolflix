6479de34aeffcf34232e6bcb35a298cb244cc5ff
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-16 12:54:02 +0100

    add store

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import { store } from './store.js';
+
+export default {
+    name: 'App',
+    components: {
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    mounted() {
+        store.callApi(`${store.url}?api_key=${store.params.api_key}&query=${store.params.query}`)
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+
--- src/store.js
+++ src/store.js
@@ -1,8 +1,27 @@
+import axios from 'axios';
+
 import {
     reactive
 
 } from 'vue'
 
 export const store = reactive({
+    url: 'https://api.themoviedb.org/3/search/company',
+    params: {
+        api_key: 'f528b95285dd507e418eb5dbedffc474',
+        query: ''
+    },
+    callApi(url) {
+        axios(url)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+
+
+    }
 
 })
