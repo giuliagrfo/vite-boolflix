52c09e6535864313901515e073aca10aaa7b22ec
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-16 13:28:12 +0100

    create component - main

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <script >
 import { store } from './store.js';
+import AppMain from './components/AppMain.vue';
 
 export default {
     name: 'App',
     components: {
+        AppMain
+
     },
     data() {
         return {
             store
         }
     },
+    methods: {
+        searchMovie() {
+            console.log('Search');
+
+            // const searchText = this.store.searchText
+            // // const url
+        }
+    },
     mounted() {
-        store.callApi(`${store.url}?api_key=${store.params.api_key}&query=${store.params.query}`)
+        this.store.callApi(`${this.store.url}?api_key=${this.store.params.api_key}&query=${this.store.params.query}`)
     }
 }
 </script>
 
 <template>
-
+    <AppMain @searchData="searchMovie" />
 </template>
 
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script >
+import { store } from '../store.js';
+
+export default {
+    name: 'AppMain',
+    emits: ['searchData'],
+    components: {
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="container">
+        <div class="search d-flex">
+            <input type="search" class="form-control" v-model="store.searchText">
+            <button type="submit" class="btn btn-primary" @click="$emit('searchData')">Search</button>
+        </div>
+
+
+    </div>
+
+</template>
